61fd49b050ee9d3f46da9051017917e3c004fb28
Aleksa Nesic <anesic9@hotmail.rs>
2021-10-15 14:47:23 +0200

    h1 change 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Testirranje"/>
+    <HelloWorld msg="Testirranje2"/>
   </div>
 </template>
 
